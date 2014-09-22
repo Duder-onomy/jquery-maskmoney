b06a6e8311f7278421ae68eebbe51b75337968a1
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2014-09-22 10:53:35 -0700

    Adding Tests for programmatic sets.

--- test/programmatic_sets_test.js
+++ test/programmatic_sets_test.js
@@ -2,9 +2,9 @@
 
 module("programmatic_sets");
 test("it correctly masks the input when the inputs value is set with jquery.val()", function() {
-    var input = $("#input1").maskMoney();
+    var $input = $("#input1").maskMoney();
 
-    input.val("5");
+    $input.val("500");
 
-    equal(input.val(), "5.00", "Failed");
+    equal($input.val(), "5.00", "Failed");
 });
